bb35d26c563dd01958b6c6ee04b9368f26d6c60f
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2022-09-16 11:56:55 +0300

    add role seeder and repository

---
+++ src/main/java/com/example/demo/seeder/RoleSeeder.java
@@ -0,0 +1,24 @@
+package com.example.demo.seeder;
+
+import com.example.demo.model.Role;
+import com.example.demo.repository.RoleRepository;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RoleSeeder {
+
+    private static final Logger log = LoggerFactory.getLogger(RoleSeeder.class);
+
+    @Bean
+    CommandLineRunner initDb(RoleRepository repo){
+        return args -> {
+            log.info("Seeding " + repo.save(new Role("normal")));
+            log.info("Seeding " + repo.save(new Role("admin")));
+        };
+    }
+
+}
